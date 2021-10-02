d0b6b8ba647794eb1c2ce6ae45ed07e9cf204b73
Dawid Lipski <Dawid.lipski99@wp.pl>
2021-10-02 18:49:20 +0200

    Pierwsze ćwiczenie

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,12 @@ public class Main {
     public static void main(String[] args) {
 //        ćw.1 napisać po kilka przykładów wypisywania tekstu w konsoli,
 //        Użyć każdej z opcji
-
+        System.out.println("Witam w pierwszym ćwiczeniu");
+        System.out.println("Próbuję" + " " + "coś zrobić" + " " + "może się uda");
+        System.out.printf("To %s zaczynam %s robić. \n", "więc", "przykład");
+        System.out.print("Jest"+ " ");
+        System.out.print("dość"+ " ");
+        System.out.print("późno");
 
     }
 }

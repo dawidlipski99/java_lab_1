1149d95a81dfbd823bc1549a24da82c97bf33c87
Dawid Lipski <Dawid.lipski99@wp.pl>
2021-10-02 20:04:14 +0200

    Trzecie ćwiczenie

--- src/Main.java
+++ src/Main.java
@@ -12,10 +12,41 @@ public class Main {
         *   b) wyświetlić dane za pomocą println i printf
         */
 
+        Scanner scan= new Scanner(System.in);
+        /*5
+        System.out.print("Podaj imię");
+        String imie = scan.next();
+        System.out.print("Podaj nazwisko");
+        String nazwisko = scan.next();
+        System.out.print("Podaj wiek");
+        int wiek = scan.nextInt();
+        System.out.print("Podaj nr indeksu");
+        int indeks = scan.nextInt();
+        System.out.printf("Więc twoje imie to: %s, a nazwisko to: %s",imie,nazwisko);
+        System.out.println("Masz " + wiek + " lat, a twój numer indeksu to:"+ indeks );
+
+         */
+
+
+
+
 
         /*  zad.2 zadeklarować 2 zmienne typu int i wykonać na nich obliczenia arytmetyczn (+, -, *, /, %)
         *   wynik wyświetlić w konsoli programu
         */
+
+        System.out.print("Podaj liczbe");
+        int a = scan.nextInt();
+        System.out.print("Podaj drugą liczbe");
+        int b = scan.nextInt();
+        int odejmowanie= a-b;
+        System.out.println("Wynik dodawania:"+ a+b);
+        System.out.print("Wynik odejmowania:"+ odejmowanie +"\n");
+        System.out.println("Wynik mnożenia:"+a*b);
+        System.out.println("Wynik dzielenia:"+a/b);
+        System.out.println("Wynik dodawania:"+a%b);
+
+
 
     }
 }
